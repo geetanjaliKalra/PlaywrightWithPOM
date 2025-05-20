9bb2b40a841a4a9994a4bd2dcae033c82c21aa6a
Geetanjali Kalra <gkalra88@gmail.com>
2025-05-20 16:46:28 +0400

    JSAlerts

---
+++ tests/JSAlerts.spec.ts
@@ -0,0 +1,51 @@
+import test, { Browser, chromium, expect, Page } from "@playwright/test";
+
+let browser: Browser;
+let page: Page;
+test("File upload test alert", async () => {
+  browser = await chromium.launch({ channel: "chrome", headless: false });
+  page = await browser.newPage();
+  await page.goto("https://the-internet.herokuapp.com/javascript_alerts");
+
+  page.on("dialog", async (alert) => {
+    expect(alert.type()).toBe("alert");
+    alert.accept();
+    expect(await page.locator("#result")).toHaveText(
+      "You successfully clicked an alert"
+    );
+    await page.waitForTimeout(5000);
+  });
+
+  await page.getByText("Click for JS Alert").click();
+});
+
+test.only("File upload test confirmation alert ", async () => {
+  browser = await chromium.launch({ channel: "chrome", headless: false });
+  page = await browser.newPage();
+  await page.goto("https://the-internet.herokuapp.com/javascript_alerts");
+
+  page.on("dialog", async (alert) => {
+    expect(alert.type()).toBe("confirm");
+    alert.accept();
+    expect(await page.locator("#result")).toHaveText("You clicked: Ok");
+    await page.waitForTimeout(5000);
+  });
+
+  await page.getByText("Click for JS Confirm").click();
+});
+
+test.only("File upload test confirmation prompt ", async () => {
+  browser = await chromium.launch({ channel: "chrome", headless: false });
+  page = await browser.newPage();
+  await page.goto("https://the-internet.herokuapp.com/javascript_alerts");
+
+  page.on("dialog", async (alert) => {
+    expect(alert.type()).toBe("prompt");
+    let text = "Geetu";
+    alert.accept(text);
+    expect(await page.locator("#result")).toHaveText(`You entered: ${text}`);
+  });
+
+  await page.getByText("Click for JS Prompt").click();
+  await page.waitForTimeout(5000);
+});
